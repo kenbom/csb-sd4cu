9a9af75217626f8cd680ee243cb3f177bc94cc36
Code Sandbox <hello@codesandbox.io>
2021-10-15 23:44:25 +0000

    Deployment from Codesandbox.

---
+++ component---src-pages-page-2-js-319ef020a00fd209d6f5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkssg=self.webpackChunkssg||[]).push([[617],{9863:function(e,t,n){n.r(t);var l=n(7294),a=n(5444),c=n(5127),o=n(3751);t.default=function(){return l.createElement(c.Z,null,l.createElement(o.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(a.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-319ef020a00fd209d6f5.js.map
